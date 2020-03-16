d0dfb32bf661e9645576fee912d11c79cbfd15bc
itsaiub <itsaiub@gmail.com>
2020-03-16 17:09:54 +0600

    init with tailwind config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col py-2 justify-center items-center">
+      <div style={{ width: "80vw" }} className="container mx-auto">
+        <h1>Heoolo</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
